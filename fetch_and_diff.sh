94a2c4a31bb1fa3a7d1693ee279d9b0ff8999a97
sirouk <sirouk>
2024-07-05 00:06:06 -0400

    added workspace

--- fetch_and_diff.sh
+++ fetch_and_diff.sh
@@ -3,8 +3,15 @@
 # requirements
 sudo apt install -y jq curl tar
 
-# remove any previous runs
-rm -rf github_source pypi_source github_*.tar.gz pypi_*.tar.gz
+# create variable for workspace
+export GHPP_WORKSPACE=$HOME/ghpp_check_tmp
+
+
+# Function to create a temp workspace for diff checking
+make_clean_diff_check_workspace() {
+  rm -rf $GHPP_WORKSPACE
+  mkdir -p $GHPP_WORKSPACE
+}
 
 # Function to fetch the specified or latest release from GitHub
 fetch_github_release() {
@@ -29,6 +36,7 @@ fetch_github_release() {
   fi
   
   local github_tarball="github_${version}.tar.gz"
+  cd $GHPP_WORKSPACE/
   curl -sL -o $github_tarball $release_url
   mkdir -p github_source
   tar -xzf $github_tarball -C github_source --strip-components=1 > /dev/null 2>&1
@@ -52,6 +60,7 @@ fetch_pypi_source() {
   fi
   
   local pypi_tarball="pypi_${version}.tar.gz"
+  cd $GHPP_WORKSPACE/
   curl -sL -o $pypi_tarball $release_url
   mkdir -p pypi_source
   tar -xzf $pypi_tarball -C pypi_source --strip-components=1 > /dev/null 2>&1
@@ -61,6 +70,9 @@ fetch_pypi_source() {
 perform_diff_check() {
   local dir=$1
   local continue_on_error=$2
+  local relname=$3
+
+  cd $GHPP_WORKSPACE/
   diff_output=$(diff -r "github_source/$dir" "pypi_source/$dir")
 
   if [ -z "$diff_output" ]; then
@@ -70,6 +82,9 @@ perform_diff_check() {
     if [ $continue_on_error = 0 ]; then
       exit 1
     fi
+    # Copy before continuing to preserve 
+    echo "Repos with diff will be kept in $GHPP_WORKSPACE-$relname-hasdiff"
+    cp -rf $GHPP_WORKSPACE $GHPP_WORKSPACE-$relname-hasdiff
   fi
 }
 
@@ -81,13 +96,13 @@ fetch_and_check_all_github_releases() {
   releases=$(curl -s "https://api.github.com/repos/$gorg/$grepo/releases" | jq -r '.[].tag_name')
 
   for release in $releases; do
-    rm -rf github_source pypi_source github_*.tar.gz pypi_*.tar.gz
+    make_clean_diff_check_workspace
     echo "Fetching GitHub release $release"
     fetch_github_release $gorg $grepo $release
     echo "Fetching PyPI source $release"
     fetch_pypi_source $prepo $release
     echo "Performing diff check on the repos"
-    perform_diff_check $grepo 1
+    perform_diff_check $grepo 1 $release
   done
 }
 
@@ -102,9 +117,12 @@ main() {
   local github_repo=$2
   local pypi_repo=$3
   local version=${4:-latest}
+
+  make_clean_diff_check_workspace
   
   if [ "$version" = "all" ]; then
     fetch_and_check_all_github_releases $github_org $github_repo $pypi_repo
+    make_clean_diff_check_workspace
   else
     echo "Fetching GitHub release from $github_org/$github_repo (version: $version)"
     fetch_github_release $github_org $github_repo $version
@@ -113,7 +131,8 @@ main() {
     fetch_pypi_source $pypi_repo $version
     
     echo "Performing diff check on the repos"
-    perform_diff_check $github_repo 0
+    perform_diff_check $github_repo 0 $version
+    make_clean_diff_check_workspace
   fi
 }
 
